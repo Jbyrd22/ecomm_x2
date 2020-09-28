6ef9f74cdc8e53a007cc7c577615aa953ed13816
Jason <jason_crask@yahoo.com>
2020-09-28 14:48:51 -0400

    adding in validator checks for auth routes

--- Routes/admin/auth.js
+++ Routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const usersRepo = require('../../repositories/users');
 const signUpTemplate = require('../../views/admin/auth/signup');
@@ -11,19 +11,29 @@ router.get('/signup', (req, res) => {
 	res.send(signUpTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
-	const foundUser = await usersRepo.getOneby({ email });
-	if (foundUser) {
-		return res.send('Email is already in use.');
+router.post(
+	'/signup',
+	[
+		check('email').trim().normalizeEmail().isEmail(),
+		check('password').trim().isLength({ min: 4, max: 20 }),
+		check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
+		const { email, password, passwordConfirmation } = req.body;
+		const foundUser = await usersRepo.getOneby({ email });
+		if (foundUser) {
+			return res.send('Email is already in use.');
+		}
+		if (password !== passwordConfirmation) {
+			return res.send('Passwords do not match!');
+		}
+		const user = await usersRepo.create({ email, password });
+		req.session.userId = user.id;
+		res.send('Account Created!');
 	}
-	if (password !== passwordConfirmation) {
-		return res.send('Passwords do not match!');
-	}
-	const user = await usersRepo.create({ email, password });
-	req.session.userId = user.id;
-	res.send('Account Created!');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
